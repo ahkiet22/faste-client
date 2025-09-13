d5619585747b5f73fa9a23f6220d65babeab4af6
ichibidouble2 <179363806+ichibidouble2@users.noreply.github.com>
2025-09-14 00:50:07 +0700

    feat: setup husky, prettier and commitlint

---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'perf',
+        'ci',
+        'build',
+        'revert',
+      ],
+    ],
+    'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
+  },
+};
+
+export default config;
